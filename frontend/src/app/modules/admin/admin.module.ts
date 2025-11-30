6a4479b0032edc5a263bba3dd144767c63552e09
Narayan Sahu <128928379+narayan439@users.noreply.github.com>
2025-12-01 01:47:42 +0530

    add

--- frontend/src/app/modules/admin/admin.module.ts
+++ frontend/src/app/modules/admin/admin.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 
 import { AdminRoutingModule } from './admin-routing.module';
@@ -35,7 +36,8 @@ import { AddTeacherComponent } from './manage-teachers/add-teacher/add-teacher.c
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatSelectModule
+    MatSelectModule,
+    MatTableModule 
   ]
 })
 export class AdminModule { }
--- frontend/src/app/modules/admin/manage-students/manage-students.component.ts
+++ frontend/src/app/modules/admin/manage-students/manage-students.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../../core/services/admin.service';
 
 @Component({
   selector: 'app-manage-students',
   templateUrl: './manage-students.component.html',
   styleUrls: ['./manage-students.component.css']
 })
-export class ManageStudentsComponent {
+export class ManageStudentsComponent implements OnInit {
+
+  displayedColumns: string[] = ['name', 'email', 'className', 'rollNo', 'actions'];
+
+  students = [
+    {
+      name: 'Narayan',
+      email: 'narayan@student.com',
+      className: '10',
+      rollNo: '23'
+    },
+    {
+      name: 'Rakesh',
+      email: 'rakesh@student.com',
+      className: '9',
+      rollNo: '19'
+    }
+  ];
+
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    // Later use backend API:
+    // this.adminService.getAllStudents().subscribe(res => this.students = res);
+  }
 
 }
